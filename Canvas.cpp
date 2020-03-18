d9a80208727d538c97a6116f4bdc0edb5ba63eb8
Wyatt Marvil <2020wmarvil@tjhsst.edu>
2020-03-17 23:15:57 -0400

    Create a function to draw text within the canvas

--- Canvas.cpp
+++ Canvas.cpp
@@ -35,9 +35,27 @@ bool Canvas::on_draw(const Cairo::RefPtr<Cairo::Context>& cr) {
         cr->move_to(x1, y1);
         cr->line_to(x2, y2);
         cr->stroke();
-
         click1 = false; click2 = false;
     } cr->restore();
 
+    cr->save();
+    cr->set_source_rgb(0.5, 0, 0.5);
+    draw_text(cr, "yeet", width, height/2);
+    cr->restore();
+
     return true;
+}
+
+void Canvas::draw_text(const Cairo::RefPtr<Cairo::Context>& cr, const std::string& str, int width, int height) {
+    Pango::FontDescription font;
+    font.set_family("Monospace");
+    font.set_weight(Pango::WEIGHT_BOLD);
+
+    int text_width, text_height;
+    auto layout = create_pango_layout(str);
+    layout->set_font_description(font);
+    layout->get_pixel_size(text_width, text_height);
+
+    cr->move_to((width-text_width)/2, (height-text_height)/2);
+    layout->show_in_cairo_context(cr);
 }
--- Canvas.h
+++ Canvas.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <string>
+
 #include <gtkmm/drawingarea.h>
 #include <gtkmm/builder.h>
 
@@ -9,6 +11,8 @@ private:
 
     bool click1, click2;
     int x1, x2, y1, y2;
+
+    void draw_text(const Cairo::RefPtr<Cairo::Context>& cr, const std::string& str, int width, int height);
 protected:
     bool on_button_press_event(GdkEventButton * event);
     bool on_draw(const Cairo::RefPtr<Cairo::Context>& cr) override;
