37bba080114c59d63347d72c1aaaae6e701bd7b3
Wyatt Marvil <2020wmarvil@tjhsst.edu>
2020-03-29 22:07:47 -0400

    Fix a bug where selecting a Block with no Lines caused a segfault

--- Canvas.cpp
+++ Canvas.cpp
@@ -17,9 +17,11 @@ bool Canvas::on_button_press_event(GdkEventButton* event) {
                 for (Block* block : blocks) {
                     if (block->isClicked(mouse_x, mouse_y)) {
                         selected_block = block;
+
                         block->translate(mouse_x, mouse_y);
-                        block->next->setSourcePoint(block->getOutgoingNodeCenterX(), block->getOutgoingNodeCenterY());
-                        block->prev->setDestPoint(block->getIncomingNodeCenterX(), block->getIncomingNodeCenterY());
+                        if (block->next) { block->next->setSourcePoint(block->getOutgoingNodeCenterX(), block->getOutgoingNodeCenterY()); }
+                        if (block->prev) { block->prev->setDestPoint(block->getIncomingNodeCenterX(), block->getIncomingNodeCenterY()); }
+
                         queue_draw();
                         break;
                     }
@@ -82,8 +84,8 @@ bool Canvas::on_motion_notify_event(GdkEventMotion* event) {
         case SELECT:
             if (selected_block) {
                 selected_block->translate(mouse_x, mouse_y);
-                selected_block->next->setSourcePoint(selected_block->getOutgoingNodeCenterX(), selected_block->getOutgoingNodeCenterY());
-                selected_block->prev->setDestPoint(selected_block->getIncomingNodeCenterX(), selected_block->getIncomingNodeCenterY());
+                if (selected_block->next) { selected_block->next->setSourcePoint(selected_block->getOutgoingNodeCenterX(), selected_block->getOutgoingNodeCenterY()); }
+                if (selected_block->prev) { selected_block->prev->setDestPoint(selected_block->getIncomingNodeCenterX(), selected_block->getIncomingNodeCenterY()); }
             } break;
         case LINE:
             for (Block* block : blocks) {
